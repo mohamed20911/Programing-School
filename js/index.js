198c4e2ee6d026a01867c57807684689ee614755
Programing <91696153+mohamed20911@users.noreply.github.com>
2021-12-01 14:50:35 +0200

    My Official Site

--- js/index.js
+++ js/index.js
@@ -17,22 +17,6 @@ signupLink.onclick = () => {
   return false;
 };
 
-// Encode an SVG element as a base64 data uri.
-function svgToBase64Image(svgElement) {
-  var div = document.createElement("div");
-  div.appendChild(svgElement.cloneNode(true));
-  var b64 = window.btoa(div.innerHTML);
-  return "data:image/svg+xml;base64," + b64;
+function myFunction() {
+  window.location.href="index.html";
 }
-var svgs = document.getElementsByTagName("svg");
-var urls = [];
-for (var i = 0; i < svgs.length; i++)
-  urls.push('url("' + svgToBase64Image(svgs[i]) + '")');
-var url = urls.join(",");
-var msg =
-  "Base64 CSS image (for cross-browser support):" +
-  "<br><br>background: " +
-  url +
-  ";";
-document.getElementById("code").innerHTML = msg;
-document.body.style.background = url;
